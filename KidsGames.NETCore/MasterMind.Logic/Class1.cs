ad1182fad21856ea429cbc8d0330ca06f65284ea
Andrew-MSFT <andrew.hall@microsoft.com>
2017-04-24 22:35:41 -0700

    Adding (mostly empty)  .NET Core project

---
+++ KidsGames.NETCore/MasterMind.Logic/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MasterMind.Logic
+{
+    public class Class1
+    {
+        public static int sum(int i, int j){
+            return i + j;
+        }
+    }
+}
---
+++ KidsGames.NETCore/MasterMind.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+//using MasterMind.Logic;
+using Xunit;
+
+namespace MasterMind.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            //var s = Class1.sum(0, 2);
+            Assert.Equal(2, 2);
+        }
+    }
+}
