03b5c2594a67965bb1eb7effb760da674c95f4dd
Andrew-MSFT <andrew.hall@microsoft.com>
2017-05-04 18:21:41 -0700

    Live updates for code setter working

--- KidsGames.NETCore/src/Mastermind/Mastermind.Web/wwwroot/js/mm-liveupdates.js
+++ KidsGames.NETCore/src/Mastermind/Mastermind.Web/wwwroot/js/mm-liveupdates.js
@@ -8,7 +8,7 @@ function onUpdateMessage(event) {
     var result = JSON.parse(event.data);
     for (var i = 0; i < result.Guesses.length; i++) {
         viewModel.guesses()[result.TurnNumber].items()[i].value(result.Guesses[i]);
-        //viewModel.guesses()[id[1]].items()[id[2]].value(newVal);
+        viewModel.results()[result.TurnNumber].items()[i].value(result.GuessResults[i]);
     }
     
 }
